51b327c9c79c4ae0238fb54106c6fece807496b3
Computer-TR <computertrinc@gmail.com>
2026-01-30 16:18:13 -0600

    Update sitemap.ts 2.0

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,12 @@
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://www.jakelplumbing.com</loc>
-    <lastmod>2026-01-30T22:06:23.899Z</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>1.0</priority>
-  </url>
-</urlset>
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.jakelplumbing.com',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ]
+}
